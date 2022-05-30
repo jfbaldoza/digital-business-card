08d60c04f6843d357affd82fdc03ad6cb284ecd9
Jan Francis Baldoza <you@example.com>
2022-05-30 07:48:57 -0400

    Updates

---
+++ precache-manifest.eea20161e48926522db7a91687708729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00438b0eba5abebd2e16",
+    "url": "/digital-business-card/static/css/main.40530f82.chunk.css"
+  },
+  {
+    "revision": "00438b0eba5abebd2e16",
+    "url": "/digital-business-card/static/js/main.00438b0e.chunk.js"
+  },
+  {
+    "revision": "30849235aa976d4a53ee",
+    "url": "/digital-business-card/static/js/1.30849235.chunk.js"
+  },
+  {
+    "revision": "2ac6fa471db2f3f58c76",
+    "url": "/digital-business-card/static/js/runtime~main.2ac6fa47.js"
+  },
+  {
+    "revision": "4ae89ad302b3a06450cc8640b1b42a73",
+    "url": "/digital-business-card/static/media/main-img.4ae89ad3.jpg"
+  },
+  {
+    "revision": "2b2dc03462fdd25ffe0aebad9ea1a06b",
+    "url": "/digital-business-card/index.html"
+  }
+];
